607905d73874cf162a69a1cb97937c250390b600
michi <michi.hostettler@cern.ch>
2019-08-26 09:43:21 +0200

    add stream processing: zip

--- src/main/java/org/ossgang/commons/observable/Observables.java
+++ src/main/java/org/ossgang/commons/observable/Observables.java
@@ -1,6 +1,18 @@
 package org.ossgang.commons.observable;
 
+import org.ossgang.commons.property.Property;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
+import java.util.function.Function;
+
+import static org.ossgang.commons.observable.ObservableValue.ObservableValueSubscriptionOption.FIRST_UPDATE;
+import static org.ossgang.commons.property.Properties.property;
 
 /**
  * Static support class for dealing with {@link Observable} and {@link ObservableValue}.
@@ -16,7 +28,7 @@ public class Observables {
      * subscribes to the upstream observable and caches its latest value.
      *
      * @param observable the observable
-     * @param <T> the type
+     * @param <T>        the type
      * @return an {@link ObservableValue} reflecting the observable provided
      */
     public static <T> ObservableValue<T> observableValueOf(Observable<T> observable) {
@@ -31,10 +43,39 @@ public class Observables {
      * return the given value on get(). On subscribe(), it will immediately send the given value to the listener.
      *
      * @param value the value
-     * @param <T> the value type
+     * @param <T>   the value type
      * @return a constant ObservableValue
      */
     public static <T> ObservableValue<T> constant(T value) {
         return new ConstantObservableValue<>(value);
+    }
+
+    /**
+     * Merges any Observables of the same type into one. Updates whenever all sources have provided at least one item.
+     */
+    @SafeVarargs
+    public static <K, V> ObservableValue<Map<K, V>> zip(Function<K, ObservableValue<V>> supplier, K... items) {
+        return zip(supplier, Arrays.asList(items));
+    }
+
+    /**
+     * Merges any Observables of the same type into one. Updates whenever all sources have provided at least one item.
+     */
+    public static <K, V> ObservableValue<Map<K, V>> zip(Function<K, ObservableValue<V>> supplier, Collection<K> items) {
+        Property<Map<K, V>> combinedProperty = property();
+        Map<K, V> values = new HashMap<>();
+        Set<K> itemSet = new HashSet<>(items);
+        for (K item : items) {
+            supplier.apply(item).subscribe(value -> {
+                synchronized (values) {
+                    values.put(item, value);
+                    if (values.keySet().containsAll(itemSet)) {
+                        combinedProperty.set(new HashMap<>(values));
+                        values.clear();
+                    }
+                }
+            }, FIRST_UPDATE);
+        }
+        return combinedProperty;
     }
 }
