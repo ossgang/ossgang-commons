57376b4fb27961e996ba40bc7c6ff428fe8818f1
michi <michi.hostettler@cern.ch>
2020-03-23 12:55:21 +0100

    add awaitNext() functionality on Observable

---
+++ src/main/java/org/ossgang/commons/observables/operators/BlockingOperators.java
@@ -0,0 +1,39 @@
+package org.ossgang.commons.observables.operators;
+
+import org.ossgang.commons.monads.Maybe;
+import org.ossgang.commons.observables.Observable;
+import org.ossgang.commons.observables.Observer;
+import org.ossgang.commons.observables.Observers;
+import org.ossgang.commons.observables.Subscription;
+
+import java.time.Duration;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+import static org.ossgang.commons.awaitables.Retry.retry;
+
+public final class BlockingOperators {
+    private BlockingOperators() {
+        throw new UnsupportedOperationException("static only");
+    }
+
+    public static <T> T awaitNextValue(Observable<T> observable, Duration timeout) {
+        return awaitNextItem(observable, timeout, consumer -> consumer::accept);
+    }
+
+    public static <T> Maybe<T> awaitNext(Observable<T> observable, Duration timeout) {
+        return awaitNextItem(observable, timeout, Observers::forMaybes);
+    }
+
+    private static <T, O> T awaitNextItem(Observable<O> observable, Duration timeout,
+                                          Function<Consumer<T>, Observer<O>> observerFactory) {
+        AtomicReference<T> update = new AtomicReference<>();
+        Subscription subscription = observable.subscribe(observerFactory.apply(update::set));
+        try {
+            return retry(update::get).untilNotNull().atMost(timeout);
+        } finally {
+            subscription.unsubscribe();
+        }
+    }
+}
