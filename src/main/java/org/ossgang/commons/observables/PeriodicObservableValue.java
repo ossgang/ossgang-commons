3729c378366b21f5f62317362bd02a33771fae4b
Kajetan Fuchsberger <K.Fuchsberger@gsi.de>
2021-02-11 14:02:52 +0100

    Using daemon threads for periodic observable

--- src/main/java/org/ossgang/commons/observables/PeriodicObservableValue.java
+++ src/main/java/org/ossgang/commons/observables/PeriodicObservableValue.java
@@ -1,9 +1,13 @@
 package org.ossgang.commons.observables;
 
+import static org.ossgang.commons.utils.NamedDaemonThreadFactory.daemonThreadFactoryWithPrefix;
+
 import java.time.Instant;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
+
+import org.ossgang.commons.utils.NamedDaemonThreadFactory;
 
 /**
  * Creates an observable value that emits periodically at the given rate. The value emitted is the current time (as
@@ -11,7 +15,7 @@ import java.util.concurrent.TimeUnit;
  */
 public class PeriodicObservableValue extends DispatchingObservableValue<Instant> {
 
-    private final ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
+    private final ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor(daemonThreadFactoryWithPrefix("periodic-observable"));
 
     PeriodicObservableValue(long period, TimeUnit unit) {
         super(Instant.now());
