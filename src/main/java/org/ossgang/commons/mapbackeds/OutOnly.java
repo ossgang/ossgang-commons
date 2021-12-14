dddb902ac7cb5c3843b135ce7dc8877796c1330d
Kajetan Fuchsberger <K.Fuchsberger@gsi.de>
2021-12-14 08:40:08 +0100

    moved OutOnly annotation to ossgang-commons

---
+++ src/main/java/org/ossgang/commons/mapbackeds/OutOnly.java
@@ -0,0 +1,15 @@
+package org.ossgang.commons.mapbackeds;
+
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+@Retention(RUNTIME)
+@Target(METHOD)
+public @interface OutOnly {
+
+}
